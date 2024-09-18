e2ec58b06fb9d65145e81406ed07bfc9c421126a
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-09-18 17:31:11 +0600

    p1

--- test2.js
+++ test2.js
@@ -1,1 +1,13 @@
 console.log("hel");
+
+class TaxCalculator {
+    static calculate(total) {
+      return total * 0.05;
+    }
+  }
+
+//   const result = calculate(20);
+//   console.log(result);
+
+let x = 0 && 'lws'
+console.log(x);
