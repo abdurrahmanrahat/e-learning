74ec234a631440fd6e43e790262cd3fea0bd5b08
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-09-20 00:21:28 +0600

    modify

--- fronted/tailwind.config.js
+++ fronted/tailwind.config.js
@@ -10,6 +10,9 @@ export default {
         primary: "#49BBBD",
         secondary: "#252641",
         myGray: "#696984"
+      },
+      boxShadow: {
+        myCustomShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"
       }
     },
   },
