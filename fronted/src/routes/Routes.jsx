f3351afac0ffbbb66aad7d35d21707ea7d68f7b9
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-09-20 20:50:15 +0600

    dashboard layout

--- fronted/src/routes/Routes.jsx
+++ fronted/src/routes/Routes.jsx
@@ -1,12 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import DashboardLayout from "../layout/DashboardLayout";
 import About from "../pages/About/About";
+import AddCourse from "../pages/Dashboard/Admin/AddCourse/AddCourse";
+import AdminDashboardHome from "../pages/Dashboard/Admin/AdminDashboardHome/AdminDashboardHome";
+import AllCourses from "../pages/Dashboard/Admin/AllCourses/AllCourses";
+import UsersManagement from "../pages/Dashboard/Admin/UsersManagement/UsersManagement";
 import Home from "../pages/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    // errorElement: <div>Error page</div>,
     children: [
       {
         index: true,
@@ -15,6 +21,28 @@ const router = createBrowserRouter([
       {
         path: "about-us",
         element: <About />,
+      },
+    ],
+  },
+  {
+    path: "/dashboard/admin/",
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true,
+        element: <AdminDashboardHome />,
+      },
+      {
+        path: "add-course",
+        element: <AddCourse />,
+      },
+      {
+        path: "all-courses",
+        element: <AllCourses />,
+      },
+      {
+        path: "users-management",
+        element: <UsersManagement />,
       },
     ],
   },
