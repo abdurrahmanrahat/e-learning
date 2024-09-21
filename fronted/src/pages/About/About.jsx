01d970e5b177756c79a67818737b7ca3b584be8c
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2024-09-22 01:03:07 +0600

    added about page(totc)

--- fronted/src/pages/About/About.jsx
+++ fronted/src/pages/About/About.jsx
@@ -1,5 +1,58 @@
+import { useEffect, useState } from "react";
+import Totc from "../../components/Home/Totc/Totc";
+import axios from 'axios';
+import CourseCard from "./CourseCard";
+import PrimaryTitle from "../../components/Ui/PrimaryTitle";
+
 const About = () => {
-  return <div>This is about page</div>;
+
+  const [popularCourses, setPopularCourses] = useState([]);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await axios.get('/popularCourses.json');
+        setPopularCourses(response.data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+
+    fetchCourses();
+  }, []);
+
+  return (
+    <div>
+      <div className="bg-[#49bbbd] text-[#2F327D] py-20 flex flex-col items-center justify-center">
+        <h1 className="text-6xl font-bold font-nunito mb-4">About Us</h1>
+        <p className="font-roboto font-medium text-lg">Home । About</p>
+      </div>
+
+      <div className="lg:my-20 my-10 container-class md:p-4">
+        <Totc />
+
+        {/* popular courses */}
+        <div className="my-10 lg:my-20">
+          <PrimaryTitle
+            headingPart1={"Our Popular"}
+            headingPart2={"Course"}
+            style={"text-center"}
+            subtext={'Most Popular Course Of This Week'}
+          />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+            {
+              popularCourses?.map(popularCourse => <CourseCard
+                key={popularCourse?.id}
+                popularCourse={popularCourse}
+              ></CourseCard>)
+            }
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  );
 };
 
 export default About;
---
+++ fronted/src/pages/About/CourseCard.jsx
@@ -0,0 +1,42 @@
+import { GoPerson } from "react-icons/go";
+import { HiUserGroup } from "react-icons/hi2";
+import { Rating } from '@smastrom/react-rating'
+
+import '@smastrom/react-rating/style.css'
+
+
+const CourseCard = ({ popularCourse }) => {
+      const { id, courseTitle, description, instructor, rating, enrollment, thumbnail, price } = popularCourse || {};
+
+      return (
+            <div className="border">
+                  <figure className="overflow-hidden relative">
+                        <img className="hover:scale-110 transition delay-100" src={thumbnail} alt="" />
+                        <div className="bg-[#49bbbd] w-fit px-6 py-2 absolute bottom-0 right-0">
+                              <span className="text-white font-medium font-nunito text-xl"><span className="text-2xl">$</span>{price}</span>
+                        </div>
+                  </figure>
+                  <div className="p-4">
+                        <span className="text-[#6E7697] flex items-center gap-2"><GoPerson className="text-xl" /> {instructor}</span>
+                        <h2 className="text-2xl text-[#2F327D] hover:text-[#00CBB8] font-bold font-nunito mb-3 transition delay-100 cursor-pointer">{courseTitle}</h2>
+                        <p className="mb-4 text-[#6E7697]">{description}</p>
+                        <div className="flex items-center justify-between ">
+                              <div className="flex items-center text-[#6E7697]">
+                                    <Rating
+                                          style={{ maxWidth: 100 }}
+                                          value={rating}
+                                          readOnly
+                                    />
+                                    <span>({rating})</span>
+                              </div>
+                              <span className="flex items-center gap-2 text-[#6E7697]"><HiUserGroup className="text-xl" /> {enrollment}</span>
+                        </div>
+                        <button className="mt-6 border-[2px] w-full text-[#2F327D] hover:text-white px-7 py-3 hover:bg-[#23BDEE] hover:border-[#23BDEE] transition-colors duration-300">
+                              View Details
+                        </button>
+                  </div>
+            </div>
+      );
+};
+
+export default CourseCard;
