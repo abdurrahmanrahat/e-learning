950c2c35b08e05ec442919891eb69eecc1daaba1
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2024-09-21 19:28:57 +0600

    added login and registration

--- fronted/src/routes/Routes.jsx
+++ fronted/src/routes/Routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import About from "../pages/About/About";
 import Home from "../pages/Home/Home";
+import Registration from "../pages/Registration/Registration";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +19,14 @@ const router = createBrowserRouter([
         element: <About />,
       },
     ],
+  },
+  {
+    path: "registration",
+    element: <Registration />
+  },
+  {
+    path: "login",
+    element: <Login />
   },
 ]);
 
