1f859816466c3192db0947d5d48b94d23d1883a7
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2024-09-22 19:35:21 +0600

    Add some reusable components and fixs some bugs

--- fronted/src/components/Ui/ReviewCard.jsx
+++ fronted/src/components/Ui/ReviewCard.jsx
@@ -1,7 +1,8 @@
 import { FaRegClock } from "react-icons/fa";
+import Rating from "./Rating";
 
 export default function ReviewCard({ review }) {
-  const { name, image, email, instructorEmail, comment, date, rating } = review;
+  const { name, image, email, comment, date, rating } = review;
 
   return (
     <div className="bg-white text-slate-500 shadow-md rounded-xl w-[70%] p-6 flex flex-col gap-6 justify-between">
@@ -10,9 +11,12 @@ export default function ReviewCard({ review }) {
           <figure className="w-20 rounded-full">
             <img className="w-full rounded-full" src={image} alt="" />
           </figure>
-          <div className="flex flex-col gap-1 justify-center items-center">
+          <div className="flex flex-col gap-1 items-start">
             <h3 className="text-xl font-medium text-slate-700">{name}</h3>
             <p className="text-sm text-slate-400">{email}</p>
+            <span>
+              <Rating value={rating}/>
+            </span>
           </div>
         </div>
         <div className="flex gap-2 items-center justify-center">
