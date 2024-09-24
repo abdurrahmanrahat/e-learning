dba512b59d397429e212a3976c5575b70e7c3daf
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-09-24 23:40:21 +0600

    role update

---
+++ fronted/src/Hooks/api/useCourses.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import useAxios from "../useAxios";
+
+export const useCourses = () => {
+    const [courses, setCourses] = useState()
+    const apiHandler = useAxios();
+
+    useEffect(() => {
+        apiHandler.get("/users").then(res => {
+            console.log(res);
+            setCourses(res.data)
+        })
+    }, [apiHandler])
+
+    return courses
+}
