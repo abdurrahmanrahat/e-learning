543fd044904b240917a231214da85e06ef5e40fe
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-09-24 20:27:49 +0600

    modify

--- fronted/src/Hooks/useAxios.jsx
+++ fronted/src/Hooks/useAxios.jsx
@@ -1,11 +1,11 @@
-import axios from 'axios'
+import axios from "axios";
 
 const instance = axios.create({
-      baseURL: "https://e-learning-backed.vercel.app/api/v1"
-})
+  baseURL: "http://localhost:5000/api/v1",
+});
 
 const useAxios = () => {
-      return instance;
+  return instance;
 };
 
 export default useAxios;
