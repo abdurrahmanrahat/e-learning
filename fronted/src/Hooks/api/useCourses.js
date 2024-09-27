ca323205a9b100e69764f0714d0cab93ab29afc9
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-09-27 21:39:18 +0600

    functionality

--- fronted/src/Hooks/api/useCourses.js
+++ fronted/src/Hooks/api/useCourses.js
@@ -1,16 +1,15 @@
 import { useEffect, useState } from "react";
 import useAxios from "../useAxios";
 
-export const useCourses = () => {
+export const useCourses = (query) => {
     const [courses, setCourses] = useState()
     const apiHandler = useAxios();
 
     useEffect(() => {
-        apiHandler.get("/courses").then(res => {
-            console.log(res);
+        apiHandler.get(`/courses?page=${query?.page}&limit=${query?.limit}&category=${query?.category}&courseDuration=${query?.duration}&searchTerm=${query?.searchTerm}`).then(res => {
             setCourses(res?.data.data)
         })
-    }, [apiHandler])
+    }, [apiHandler, query?.page, query?.limit, query?.category, query?.duration, query?.searchTerm])
 
     return courses
 }
