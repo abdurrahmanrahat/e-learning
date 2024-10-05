c92fdfac3f357f366f58c535b330ffd0c6ddadb6
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-10-05 17:48:47 +0600

    notes

---
+++ fronted/src/utils/notes.js
@@ -0,0 +1,19 @@
+/**
+
+------------ instructors info: 
+
+1. email: john.williams@eduplatform.com password: User123
+2. email: laura.anderson@skillacademy.com password: User234
+3. email: emily.davis@learnpro.org password: User345
+4. email: michael.thompson@txpert.com password: User456
+
+**/
+
+/**
+
+------------ admin info: 
+
+1. email: rahat.admin@gmail.com password: 123456
+
+
+**/
