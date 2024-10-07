8e8fe03e3627943d6b7d8223a4c4344226c3b516
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2024-10-08 01:07:03 +0600

    Updated code

--- fronted/src/pages/Dashboard/Instructor/MyCourses/TableCourse.jsx
+++ fronted/src/pages/Dashboard/Instructor/MyCourses/TableCourse.jsx
@@ -1,43 +1,63 @@
+import { useState } from "react";
 import Rating from "../../../../components/Ui/Rating";
+import ModuleForm from "./ModuleForm";
+import Modal from "../../../../components/Ui/Modal";
 
 const TableCourse = ({ course, idx }) => {
+  const [openModal, setOpenModal] = useState(false);
+
   return (
-    <tr className="border-b border-slate-200 hover:bg-gray-50">
-      <td className="h-12 px-6 py-3 text-sm transition duration-300 border-slate-200">
-        {idx + 1}
-      </td>
+    <>
+      <tr className="border-b border-slate-200 hover:bg-gray-50">
+        <td className="h-12 px-6 py-3 text-sm transition duration-300 border-slate-200">
+          {idx + 1}
+        </td>
 
-      <td className="text-sm transition duration-300 border-slate-200">
-        <img
-          className="w-[80px] h-[60px] rounded-lg "
-          src={course?.image}
-          alt=""
-        />
-      </td>
+        <td className="text-sm transition duration-300 border-slate-200">
+          <img
+            className="w-[80px] h-[60px] rounded-lg "
+            src={course?.image}
+            alt=""
+          />
+        </td>
 
-      <td className="h-12 px-6 py-3 text-sm transition duration-300 border-slate-200">
-        <p className="text-[#2F327D] font-medium">{course?.title}</p>
-        <span className="text-slate-500">{course?.category}</span>
-      </td>
+        <td className="h-12 px-6 py-3 text-sm transition duration-300 border-slate-200">
+          <p className="text-[#2F327D] font-medium">{course?.title}</p>
+          <span className="text-slate-500">{course?.category}</span>
+        </td>
 
-      <td className="h-12 px-6 py-3 text-sm transition duration-300 border-slate-200">
-        {course?.instructorName}
-      </td>
+        <td className="h-12 px-6 py-3 text-sm transition duration-300 border-slate-200">
+          {course?.instructorName}
+        </td>
 
-      <td className="h-12 px-6 py-3 text-sm transition duration-300 border-slate-200">
-        ${course?.price}
-      </td>
+        <td className="h-12 px-6 py-3 text-sm transition duration-300 border-slate-200">
+          ${course?.price}
+        </td>
 
-      <td className="h-12 px-6 py-3 text-sm transition duration-300 border-slate-200">
-        {course?.courseDuration}
-        <span className="flex gap-1 justify-start items-center text-[#6E7697] mb-6">
-          <Rating value={course?.totalRatings} />
-          <span className="text-sm text-[#969696]">
-            ({course?.totalRatings})
+        <td className="h-12 px-6 py-3 text-sm transition duration-300 border-slate-200">
+          {course?.courseDuration}
+          <span className="flex gap-1 justify-start items-center text-[#6E7697] mb-6">
+            <Rating value={course?.totalRatings} />
+            <span className="text-sm text-[#969696]">
+              ({course?.totalRatings})
+            </span>
           </span>
-        </span>
-      </td>
-    </tr>
+        </td>
+        <td className="h-12 px-6 py-3 text-sm transition duration-300 border-slate-200">
+          <span
+            onClick={() => setOpenModal(true)}
+            className="cursor-pointer hover:text-primary transition-all duration-300 ease-in-out"
+          >
+            Add Module
+          </span>
+        </td>
+      </tr>
+      { openModal &&
+        <Modal openModal={openModal} setOpenModal={setOpenModal}>
+          <ModuleForm/>
+        </Modal>
+      }
+    </>
   );
 };
 
